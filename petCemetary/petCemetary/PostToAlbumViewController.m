f49fdbebfb8b434272f5d7975db69287f386e670
Melanie27 <melaniemcganney@gmail.com>
2016-11-21 10:44:31 -0800

    post to album unfinished

--- petCemetary/petCemetary/PostToAlbumViewController.m
+++ petCemetary/petCemetary/PostToAlbumViewController.m
@@ -5,8 +5,10 @@
 //  Created by MELANIE MCGANNEY on 11/19/16.
 //  Copyright © 2016 melaniemcganney.com. All rights reserved.
 //
-
+#import <Photos/Photos.h>
 #import "PostToAlbumViewController.h"
+@import Firebase;
+@import FirebaseDatabase;
 
 @interface PostToAlbumViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UIDocumentInteractionControllerDelegate>
 
@@ -16,6 +18,10 @@
     @property (nonatomic, strong) UIButton *sendButton;
     @property (nonatomic, strong) UIBarButtonItem *sendBarButton;
     @property (nonatomic, strong) UIDocumentInteractionController *documentController;
+
+    @property (nonatomic, strong) PHFetchResult *result;
+    @property (strong, nonatomic) FIRDatabaseReference *ref;
+    @property (strong, nonatomic) FIRStorage *storage;
 
 @end
 
@@ -28,9 +34,8 @@
         self.sourceImage = sourceImage;
         self.previewImageView = [[UIImageView alloc] initWithImage:self.sourceImage];
         
-        
         self.sendButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        self.sendButton.backgroundColor = [UIColor colorWithRed:0.345 green:0.318 blue:0.424 alpha:1]; /*#58516c*/
+        self.sendButton.backgroundColor = [UIColor colorWithRed:0.62 green:0.138 blue:0.02 alpha:1]; /*#58516c*/
         self.sendButton.layer.cornerRadius = 5;
         [self.sendButton setAttributedTitle:[self sendAttributedString] forState:UIControlStateNormal];
         [self.sendButton addTarget:self action:@selector(sendButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
@@ -54,12 +59,10 @@
         self.navigationItem.rightBarButtonItem = self.sendBarButton;
     }
     
-    //[self.filterCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
-    
     self.view.backgroundColor = [UIColor whiteColor];
-    //self.filterCollectionView.backgroundColor = [UIColor whiteColor];
+   
     
-    self.navigationItem.title = NSLocalizedString(@"Apply Filter", @"apply filter view title");
+    self.navigationItem.title = @"Post to Pet Album";
     
     
 }
@@ -71,18 +74,16 @@
     
     self.previewImageView.frame = CGRectMake(0, self.topLayoutGuide.length, edgeSize, edgeSize);
     
-    CGFloat buttonHeight = 50;
-    CGFloat buffer = 10;
+    CGFloat buttonHeight = 70;
+    CGFloat buffer = 20;
     
-   // CGFloat filterViewYOrigin = CGRectGetMaxY(self.previewImageView.frame) + buffer;
-    //CGFloat filterViewHeight;
     
     if (CGRectGetHeight(self.view.frame) > 500) {
         self.sendButton.frame = CGRectMake(buffer, CGRectGetHeight(self.view.frame) - buffer - buttonHeight, CGRectGetWidth(self.view.frame) - 2 * buffer, buttonHeight);
         
-        //filterViewHeight = CGRectGetHeight(self.view.frame) - filterViewYOrigin - buffer - buffer - CGRectGetHeight(self.sendButton.frame);
+        
     } else {
-        //filterViewHeight = CGRectGetHeight(self.view.frame) - CGRectGetMaxY(self.previewImageView.frame) - buffer - buffer;
+        
     }
     
    
@@ -99,7 +100,7 @@
     
     NSMutableAttributedString *commentString = [[NSMutableAttributedString alloc] initWithString:baseString];
     
-    [commentString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:13] range:range];
+    [commentString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:14] range:range];
     [commentString addAttribute:NSKernAttributeName value:@1.3 range:range];
     [commentString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1] range:range];
     
@@ -107,6 +108,85 @@
 }
 
 - (void) sendButtonPressed:(id)sender {
+    
+    //TODO Post image url to Firebase with the correct UID and includ ability to add a caption
+    FIRUser *userAuth = [FIRAuth auth].currentUser;
+    self.ref = [[FIRDatabase database] reference];
+    
+    PHAsset *asset = nil;
+    
+    //if (self.result[indexPath.row] != nil && self.result.count > 0) {
+        //asset = self.result[indexPath.row];
+    //}
+    
+    
+    PHImageRequestOptions *imageRequestOptions = [[PHImageRequestOptions alloc] init];
+    imageRequestOptions.synchronous = YES;
+    [[PHImageManager defaultManager]
+     requestImageDataForAsset:asset
+     options:imageRequestOptions
+     resultHandler:^(NSData *imageData, NSString *dataUTI,
+                     UIImageOrientation orientation,
+                     NSDictionary *info)
+     {
+         
+         if ([info objectForKey:@"PHImageFileURLKey"]) {
+             
+             NSURL *localURL = [info objectForKey:@"PHImageFileURLKey"];
+             NSString *localURLString = [localURL path];
+             NSString *key = localURLString;
+             NSString *theFileName = [[key lastPathComponent] stringByDeletingPathExtension];
+             
+             
+             //UPLOAD TO FB
+             FIRStorage *storage = [FIRStorage storage];
+             FIRStorageReference *storageRef = [storage referenceForURL:@" gs://petcemetary-5fec2.appspot.com/petAlbums"];
+             FIRStorageReference *albumRef = [storageRef child:theFileName];
+             
+             FIRStorageUploadTask *uploadTask = [albumRef putFile:localURL metadata:nil completion:^(FIRStorageMetadata* metadata, NSError* error) {
+                 if (error != nil) {
+                     // Uh-oh, an error occurred!
+                     //NSLog(@"error %@", error);
+                 } else {
+                     // Metadata contains file metadata such as size, content-type, and download URL.
+                     NSURL *downloadURL = metadata.downloadURL;
+                     NSString *downloadURLString = [ downloadURL absoluteString];
+                     
+                     
+                     //push the selected photo to database
+                     FIRDatabaseQuery *pathStringQuery = [[self.ref child:[NSString stringWithFormat:@"/photos/%@/", userAuth.uid]] queryLimitedToFirst:100];
+                     
+                     [pathStringQuery
+                      observeEventType:FIRDataEventTypeValue
+                      withBlock:^(FIRDataSnapshot *snapshot) {
+                          //TODO - not seeing the correct image --WHAT IS THIS CODE DOING?? DO I NEED IT?
+                          //static NSInteger imageViewTag = 54321;
+                          //UIImageView *imgView = (UIImageView*)[[collectionView cellForItemAtIndexPath:indexPath] viewWithTag:imageViewTag];
+                          //UIImage *img = imgView.image;
+                          //[[BLCDataSource sharedInstance] setUserImage:img];
+                          
+                          
+                      }];
+                     
+                     NSDictionary *childUpdates = @{[NSString stringWithFormat:@"/userData/%@/profile_picture/", userAuth.uid]:downloadURLString};
+                     
+                     [_ref updateChildValues:childUpdates];
+                     
+                     
+                 }
+             }];
+             
+             
+             
+             
+             
+         }
+     }];
+    
+    
+    //[self cancelPressed:self.navigationItem.leftBarButtonItem];
+    
+    
     NSURL *instagramURL = [NSURL URLWithString:@"instagram://location?id=1"];
     
     UIAlertController *alertVC;
