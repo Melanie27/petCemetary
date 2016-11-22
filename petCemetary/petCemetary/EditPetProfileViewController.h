51356605b25caf6e94196c0221d27228342e5330
Melanie27 <melaniemcganney@gmail.com>
2016-11-22 13:41:28 -0800

    restructure some VCs

---
+++ petCemetary/petCemetary/EditPetProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditPetProfileViewController.h
+//  petCemetary
+//
+//  Created by MELANIE MCGANNEY on 11/22/16.
+//  Copyright © 2016 melaniemcganney.com. All rights reserved.
+//
+
+#import "AddPetProfileViewController.h"
+
+@interface EditPetProfileViewController : AddPetProfileViewController
+
+@end
---
+++ petCemetary/petCemetary/EditPetProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  EditPetProfileViewController.m
+//  petCemetary
+//
+//  Created by MELANIE MCGANNEY on 11/22/16.
+//  Copyright © 2016 melaniemcganney.com. All rights reserved.
+//
+
+#import "EditPetProfileViewController.h"
+
+@interface EditPetProfileViewController ()
+
+@end
+
+@implementation EditPetProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Edit Profile";
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
