c6e4fb5073a27b7d4412fa86d18ff8300313451d
Melanie27 <melaniemcganney@gmail.com>
2016-11-30 12:58:15 -0800

    cleaning

--- petCemetary/petCemetary/AddPetProfileViewController.m
+++ petCemetary/petCemetary/AddPetProfileViewController.m
@@ -88,26 +88,7 @@
     NSLog(@"this method should save the pet to the logged in user");
     [self sendPetInfoToFirebase];
     
-    /*NSString *savedPetName = self.petNameTextField.text;
-    NSString *savedAnimalType = self.animalTypeTextField.text;
-    NSString *savedAnimalBreed = self.animalBreedTextField.text;
-    NSString *savedPersonality = self.animalPersonalityTextView.text;
-    NSString *savedOwnerName = self.ownerNameTextField.text;
-    NSString *petImageString = @"https://firebasestorage.googleapis.com/v0/b/petcemetary-5fec2.appspot.com/o/petFeed%2Fspooky.png?alt=media&token=58e1b0af-a087-4028-a208-90ff8622f850";
-    self.ref = [[FIRDatabase database] reference];
-    FIRUser *userAuth = [FIRAuth auth].currentUser;
-    NSDictionary *childUpdates = @{
-                                   
-                                   [NSString stringWithFormat:@"/pets/%ld/pet/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:savedPetName,
-                                   [NSString stringWithFormat:@"/pets/%ld/animalType/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:savedAnimalType,
-                                   [NSString stringWithFormat:@"/pets/%ld/breed/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:savedAnimalBreed,
-                                   [NSString stringWithFormat:@"/pets/%ld/personality/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:savedPersonality,
-                                   [NSString stringWithFormat:@"/pets/%ld/ownerName/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:savedOwnerName,
-                                   [NSString stringWithFormat:@"/pets/%ld/feedPhoto/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:petImageString,
-                                   [NSString stringWithFormat:@"/pets/%ld/UID/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:userAuth.uid
-                                   };
     
-    [_ref updateChildValues:childUpdates];*/
     
     
 }
@@ -150,7 +131,6 @@
      //FIRStorageMetadata *metadata = [[FIRStorageMetadata alloc] init];
     FIRStorage *storage = [FIRStorage storage];
     FIRStorageReference *storageRef = [storage referenceForURL:@"gs://petcemetary-5fec2.appspot.com/petFeed/"];
-   
     
     NSString *localURLString = [docPathUrl path];
     NSString *theFileName = [[localURLString lastPathComponent] stringByDeletingPathExtension];
@@ -160,7 +140,7 @@
      //FIRStorageUploadTask *uploadTask = [storageRef putFile:docPathUrl metadata:nil];
     //NSLog(@"upload tast %@", uploadTask);
     
-    /*FIRStorageUploadTask *uploadTask = [profileRef putFile:docPathUrl metadata:metadata completion:^(FIRStorageMetadata *metadata, NSError *error) {
+    FIRStorageUploadTask *uploadTask = [profileRef putFile:docPathUrl metadata:nil completion:^(FIRStorageMetadata *metadata, NSError *error) {
         if (error != nil) {
             // Uh-oh, an error occurred!
             NSLog(@"error %@", error);
@@ -169,17 +149,17 @@
             //NSURL *downloadURL = metadata.downloadURL;
             NSLog(@"no error %@", metadata);
         }
-    }];*/
+    }];
     
-    //NSString *petImagePath  = [NSString stringWithFormat:@"%@", petImageURL];
-     //NSLog(@"pet string %@",petImagePath);
-    /*NSDictionary *childUpdates = @{
+    NSString *petImagePath  = [NSString stringWithFormat:@"%@", petImageURL];
+     NSLog(@"pet string %@",petImagePath);
+    NSDictionary *childUpdates = @{
      
-                                   [NSString stringWithFormat:@"/pets/%ld/feedPhoto/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:petImageURL,
-                                   [NSString stringWithFormat:@"/pets/%ld/UID/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:userAuth.uid
+                                   [NSString stringWithFormat:@"/pets/%ld/feedPhoto/", (unsigned long)[PCDataSource sharedInstance].petItems.count]:petImageURL
+                                   
                                    };
     
-    [_ref updateChildValues:childUpdates];*/
+    [_ref updateChildValues:childUpdates];
    
 }
 @end
