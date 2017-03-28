2ef04ab3b63256d5355432829ca0d0d0470f168f
Melanie27 <melaniemcganney@gmail.com>
2017-03-28 14:50:12 -0700

    edit pet profile constraints update

--- petCemetary/petCemetary/AddPetProfileViewController.m
+++ petCemetary/petCemetary/AddPetProfileViewController.m
@@ -25,6 +25,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"Add Pet";
+    [self.animalPersonalityTextView setReturnKeyType:UIReturnKeyDone];
 
 }
 
