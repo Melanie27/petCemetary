2a2eb3ab23f18a5ae127793974f44a582fab92d6
Melanie27 <melaniemcganney@gmail.com>
2017-03-30 14:33:09 -0700

    some auto layout updates

--- petCemetary/petCemetary/AddPetProfileViewController.m
+++ petCemetary/petCemetary/AddPetProfileViewController.m
@@ -29,52 +29,42 @@
     // Do any additional setup after loading the view.
     self.title = @"Add Pet";
     [self.animalPersonalityTextView setReturnKeyType:UIReturnKeyDone];
+  
     self.animalPersonalityTextView.delegate = self;
+   
 
 }
 
 -(void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     [self.scrollView layoutIfNeeded];
-    //self.scrollView.contentSize = self.contentView.bounds.size;
 }
 
 
 
-/*- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self.view endEditing:YES];
-}*/
-
 -(void)keyboardWillShow {
     // Animate the current view out of the way
-    if (self.view.frame.origin.y >= 0)
-    {
+    if (self.view.frame.origin.y >= 0) {
         [self setViewMovedUp:YES];
     }
-    else if (self.view.frame.origin.y < 0)
-    {
+    else if (self.view.frame.origin.y < 0) {
         [self setViewMovedUp:NO];
     }
 }
 
 -(void)keyboardWillHide {
-    if (self.view.frame.origin.y >= 0)
-    {
+    if (self.view.frame.origin.y >= 0) {
         [self setViewMovedUp:YES];
     }
-    else if (self.view.frame.origin.y < 0)
-    {
+    else if (self.view.frame.origin.y < 0){
         [self setViewMovedUp:NO];
     }
 }
 
--(void)textViewDidBeginEditing:(UITextView *)sender
-{
-    if ([sender isEqual:@"\n"])
-    {
+-(void)textViewDidBeginEditing:(UITextView *)sender {
+    if ([sender isEqual:@"\n"]) {
         //move the main view, so that the keyboard does not hide it.
-        if  (self.view.frame.origin.y >= 0)
-        {
+        if  (self.view.frame.origin.y >= 0) {
             [self setViewMovedUp:YES];
         }
     }
@@ -89,26 +79,32 @@
     return YES;
 }
 
+- (BOOL)textField:(UITextView *)textField shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    
+    if([text isEqualToString:@"\n"]) {
+        [textField resignFirstResponder];
+        return NO;
+    }
+    
+    return YES;
+}
+
 //we want to move view up/down only on text view
 //method to move the view up/down whenever the keyboard is shown/dismissed
--(void)setViewMovedUp:(BOOL)movedUp
-{
+-(void)setViewMovedUp:(BOOL)movedUp {
     [UIView beginAnimations:nil context:NULL];
     [UIView setAnimationDuration:0.3]; // if you want to slide up the view
     
     CGRect rect = self.view.frame;
-    if (movedUp)
-    {
+    if (movedUp) {
         // 1. move the view's origin up so that the text field that will be hidden come above the keyboard
         // 2. increase the size of the view so that the area behind the keyboard is covered up.
         
         rect.origin.y -= kOFFSET_FOR_KEYBOARD;
         rect.size.height += kOFFSET_FOR_KEYBOARD;
     }
-    else
-    {
+    else {
         // revert back to the normal state.
-        
         rect.origin.y += kOFFSET_FOR_KEYBOARD;
         rect.size.height -= kOFFSET_FOR_KEYBOARD;
     }
@@ -118,8 +114,7 @@
 }
 
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     // register for keyboard notifications
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -133,8 +128,7 @@
                                                object:nil];
 }
 
-- (void)viewWillDisappear:(BOOL)animated
-{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     // unregister for keyboard notifications while not visible.
     [[NSNotificationCenter defaultCenter] removeObserver:self
